7368558c9d7a255efe9c9e8515c9a56116944e0d
Quyet's Code <quyetcoccoc@gmail.com>
2025-02-01 00:21:13 +0700

    [init] setup typescript works correctly with shadcnui

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 // @ts-ignore
-import tailwindcss from '@tailwindcss/vite'
-
+// import tailwindcss from '@tailwindcss/vite'
+// import autoprefixer from 'autoprefixer'
+import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   main: {
@@ -16,10 +17,12 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@': fileURLToPath(new URL('./src/renderer/src', import.meta.url))
+        // '@': resolve('/src/renderer/src')
       }
     },
-    plugins: [vue(), tailwindcss(), vueDevTools()],
+    plugins: [vue(), vueDevTools()],
     server: {
       port: 3000,
       headers: {
--- src/renderer/src/pages/Home.vue
+++ src/renderer/src/pages/Home.vue
@@ -2,9 +2,13 @@
 import { useCounterStore } from '../stores/couter'
 import { RouterLink } from 'vue-router'
 const store = useCounterStore()
-import { axiosMainApi } from '@renderer/api/axios.expressApi';
-import axios from 'axios';
+// import { axiosMainApi } from '@renderer/api/axios.expressApi';
+// import axios from 'axios';
 import { onMounted } from 'vue';
+import { Button } from "@/components/ui/button"
+import { Input } from '@/components/ui/input';
+import { Calendar } from '@/components/ui/calendar';
+import { Badge } from '@/components/ui/badge';
 
 const fetchData = async () => {
     try {
@@ -29,10 +33,21 @@ onMounted(() => {
         <h1>Counter</h1>
         <p class="text-blue-500 text-2xl">This is</p>
         <button @click="store.add">Count {{ store.count }}</button>
-        <button @click="store.subtract">Subtract</button>
+        <Button @click="store.subtract" variant="default" size="default"
+            class="cursor-pointer hover:bg-red-700">Subtract</Button>
 
         <div>
             <RouterLink to="/contact"> Contact </RouterLink>
+        </div>
+        <div class="p-8 w-min rounded bg-slate-200">
+            <h1>Thgis is h1</h1>
+            <Button variant="secondary">Click me</Button>
+            <Input />
+        </div>
+        <div>
+            <Badge>Badge</Badge>
+            <Calendar />
+
         </div>
     </div>
 </template>
